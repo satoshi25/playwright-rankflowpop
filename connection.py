cc4fba27c3a1bccff21dc538b5778c76c51f2c37
satoshi25 <81741466+satoshi25@users.noreply.github.com>
2023-09-11 21:54:11 +0900

    feat: Add DB connection - connection.py: dotenv로 DB 연결 - requirements.txt: pymysql, dotenv, aiohttp module 추가

---
+++ connection.py
@@ -0,0 +1,14 @@
+from dotenv import load_dotenv
+import pymysql
+import os
+
+load_dotenv()
+
+db = pymysql.connect(
+    host=os.getenv("DB_HOST"),
+    user=os.getenv("DB_USER"),
+    password=os.getenv("DB_PASSWORD"),
+    port=int(os.getenv("DB_PORT")),
+    db=os.getenv("DB_NAME"),
+    charset=os.getenv("DB_CHARSET")
+)
