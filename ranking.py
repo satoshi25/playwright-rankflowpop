a3c8290a0c5ef247fa1d3fe321755cdde1e9de59
satoshi25 <81741466+satoshi25@users.noreply.github.com>
2023-09-12 23:59:13 +0900

    feat: Add get_market_name method - user가 등록한 product_url을 통해 product의 name과 market의 name을 가져오는 과정 추가

--- ranking.py
+++ ranking.py
@@ -1,4 +1,8 @@
 from connection import db
+from bs4 import BeautifulSoup
+
+import aiohttp
+import asyncio
 
 
 def get_conditions():
@@ -39,3 +43,35 @@ def get_conditions():
 
 # ===========================================================================================
 
+def get_market_url(p_list):
+    for p in p_list:
+        p_url = p["p_url"]
+        m_profile = "profile?cp=1"
+        p["m_url"] = f"{p_url.split('products')[0]}{m_profile}"
+    return p_list
+
+
+async def get_market_name(products):
+    async with aiohttp.ClientSession() as session:
+        m_url_results = await asyncio.gather(
+            *[condition_fetcher(session, product) for product in products]
+        )
+        return m_url_results
+
+
+async def condition_fetcher(session, product):
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) "
+                      "AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36"
+    }
+    async with session.get(product["m_url"], headers=headers, ssl=False) as response:
+        if response.status == 200:
+            html = await response.text()
+            soup = BeautifulSoup(html, "html.parser")
+            product["m_name"] = soup.find("strong", class_="name").text
+    async with session.get(product["p_url"], headers=headers, ssl=False) as response:
+        if response.status == 200:
+            html = await response.text()
+            soup = BeautifulSoup(html, "html.parser")
+            product["p_name"] = soup.find("h3", class_="_22kNQuEXmb _copyable").text
+            return product
