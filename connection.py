799d8ec8af7a79287e0a1018f943d62748890d6f
satoshi25 <81741466+satoshi25@users.noreply.github.com>
2023-09-14 22:06:21 +0900

    feat: Add insert_product_ranking method - product의 ranking을 DB에 insert 하는 insert_product_ranking method 추가 - record 되지 않은 user_product들을 가져오는 sql문으로 수정 - product_dict에 user별로 ranking을 가져오고 insert 하도록 그룹화

--- connection.py
+++ connection.py
@@ -1,6 +1,7 @@
-from dotenv import load_dotenv
 import pymysql
 import os
+from dotenv import load_dotenv
+
 
 load_dotenv()
 
@@ -15,26 +16,23 @@ db = pymysql.connect(
 
 
 def get_conditions():
-    sql = ("SELECT upk.user_id, upk.id, p.product_url, p.product_name, k.keyword "
-           "FROM user_product_keyword as upk "
+    sql = ("SELECT upk.user_id, upk.id, p.product_url, p.product_name, p.store_name, k.keyword "
+           "FROM user_product_keyword AS upk "
            "JOIN product_keyword pk ON upk.product_keyword_id = pk.id "
            "JOIN product p ON pk.product_id = p.id "
            "JOIN keyword k ON pk.keyword_id = k.id "
+           "LEFT JOIN record r ON r.user_product_keyword_id = upk.id AND r.search_date = CURDATE() "
+           "WHERE r.user_product_keyword_id IS NULL "
            "ORDER BY upk.user_id ASC;")
 
-    # sql = ("SELECT upk.user_id, upk.id, p.product_url, p.product_name, p.store_name, k.keyword "
-    #        "FROM user_product_keyword as upk "
-    #        "JOIN product_keyword pk ON upk.product_keyword_id = pk.id "
-    #        "JOIN product p ON pk.product_id = p.id "
-    #        "JOIN keyword k ON pk.keyword_id = k.id "
-    #        "WHERE upk.user_id = 1 "
-    #        "ORDER BY upk.user_id ASC;")
     cursor = db.cursor()
     cursor.execute(sql)
     p_list = cursor.fetchall()
 
-    product_list = []
+    product_dict = {}
     for item in p_list:
+        if item[0] not in product_dict:
+            product_dict[item[0]] = []
         data = {
             "user_id": item[0],
             "upk_id": item[1],
@@ -45,8 +43,18 @@ def get_conditions():
             "ranking": -1,
             "result": [],
         }
-        product_list[item[0]].append(data)
-    return product_list
+        product_dict[item[0]].append(data)
+    return product_dict
 
 
 # ===========================================================================================
+
+
+def insert_product_ranking(products_ranking, today):
+    sql_insert_data: str = \
+        "INSERT INTO rankflow.record (user_product_keyword_id, search_date, ranking) VALUES (%s, %s, %s)"
+    cursor = db.cursor()
+
+    for product in products_ranking:
+        cursor.execute(sql_insert_data, (product.get("upk_id"), today, product.get("ranking")))
+        db.commit()
