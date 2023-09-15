5aa82fe47afa6a8e0eaf7547d62ce25443a01abc
satoshi25 <81741466+satoshi25@users.noreply.github.com>
2023-09-15 14:07:18 +0900

    feat: Add scheduler - main.py: 일정한 시간에 동작하도록 scheduler 추가 - logger.py: app 동작 log를 볼 수 있게 app.log 추가 - requirements.txt: APScheduler module 추가

--- logger.py
+++ logger.py
@@ -2,13 +2,34 @@ import logging
 
 
 log_file = "error.log"
-logger = logging.getLogger("error_logger")
-logger.setLevel(logging.ERROR)
+error_logger = logging.getLogger("error_logger")
+error_logger.setLevel(logging.ERROR)
 
 log_handler = logging.FileHandler(log_file)
 log_handler.setLevel(logging.ERROR)
 
-formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
+base_format = "%(asctime)s - %(levelname)s - %(message)s"
+
+formatter = logging.Formatter(base_format)
 log_handler.setFormatter(formatter)
 
-logger.addHandler(log_handler)
+error_logger.addHandler(log_handler)
+
+
+# ======================================================================================
+
+
+logging.basicConfig(filename="app.log", level=logging.INFO, format=base_format)
+
+app_logger = logging.getLogger("app_logger")
+
+
+# ======================================================================================
+
+def convert_time_format(sec):
+    sec %= 24 * 3600
+    hour = sec // 3600
+    sec %= 3600
+    minute = sec // 60
+    sec %= 60
+    return f"{hour}시간 {minute}분 {sec}초"
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from datetime import date
 from dotenv import load_dotenv
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from pytz import timezone
 
 import asyncio
 import time
@@ -7,10 +9,14 @@ import os
 
 from connection import get_conditions, insert_product_ranking
 from ranking import get_page_list, html_parsing, calculate_ranking
-from logger import logger
+from logger import error_logger, app_logger, convert_time_format
 
 
 load_dotenv()
+
+server_timezone = timezone("Asia/Seoul")
+hour = os.getenv("TASK_HOUR")
+minute = os.getenv("TASK_MINUTE")
 
 
 async def main():
@@ -34,19 +40,26 @@ async def main():
                 product_ranking_data = calculate_ranking(parsing_data)
 
                 insert_product_ranking(product_ranking_data, today)
-                print(f"user {product_ranking_data[0]['user_id']} DB insert 완료")
+                app_logger.info(f"user {product_ranking_data[0]['user_id']} DB insert 완료")
 
                 is_error = False
                 await asyncio.sleep(interval)
 
         except Exception as error:
-            logger.error(f"Error: {error}", exc_info=True)
+            error_logger.error(f"Error: {error}", exc_info=True)
             error_cnt += 1
             is_error = True
             await asyncio.sleep(interval*2)
 
     end = time.time()
-    print(f"{today} DB insert 완료, 소요 시간: {end - start}")
+    running_time = convert_time_format(start - end)
+    app_logger.info(f"{today} DB insert 완료, 소요 시간: {running_time}")
+
+
+scheduler = AsyncIOScheduler()
+scheduler.add_job(main, 'cron', hour=hour, minute=minute, timezone=server_timezone)
+scheduler.start()
+
 
 if __name__ == "__main__":
-    asyncio.run(main(), debug=True)
+    asyncio.get_event_loop().run_forever()
